e19edf30a5a3c4f6cb060265a56bc23951519934
kevin <biantuadikevin@gmail.com>
2024-05-02 14:20:50 +0200

    ✅ chore: Add Vercel configuration file and update package.json script for deployment

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './schemas/user.schema';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Get("all")
+    async findAll(): Promise<User[]> {
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string): Promise<User> {
+        return this.userService.findOneById(id);
+    }
+}
+
