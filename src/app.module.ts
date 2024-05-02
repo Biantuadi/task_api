0f1866edd24c9281fc322d61b91a40fec3c40a1b
kevin <biantuadikevin@gmail.com>
2024-05-02 18:38:50 +0200

    ✅ feat: Add task and group modules
    The code changes include adding the task and group modules to the application. This allows for the management of tasks and groups within the system.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { UserModule } from './user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './user/auth/auth.controller';
 import { AuthService } from './user/auth/auth.service';
+import { TaskModule } from './task/task.module';
+import { GroupModule } from './group/group.module';
 
 @Module({
   imports: [
@@ -15,6 +17,8 @@ import { AuthService } from './user/auth/auth.service';
       signOptions: { expiresIn: '1d' }, // Optionnel : durée de validité du token
     }),
     UserModule,
+    TaskModule,
+    GroupModule,
   ],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
